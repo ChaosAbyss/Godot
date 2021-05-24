553c8beb5b53f2b6b9fef582e34bb33c050ac775
ChaosAbyss <merkulov.vadim1@yandex.ru>
2021-05-24 16:36:34 +0300

    Added first Godot project.

---
+++ 3DProject/Scripts/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody
+
+const GRAVITY = -10
+const SPEED = 400
+const ROTATION = 3
+var vel = Vector3()
+
+func _process(delta):
+	var dir = Vector3()
+	
+	if Input.is_action_pressed("ui_left"):
+#		dir.x = -1
+		rotate_y(ROTATION * delta)
+	if Input.is_action_pressed("ui_right"):
+#		dir.x = 1
+		rotate_y(-ROTATION * delta)
+	if Input.is_action_pressed("ui_up"):
+		dir.z = -1
+	if Input.is_action_pressed("ui_down"):
+		dir.z = 1
+
+	if dir:
+		dir *= SPEED * delta
+		dir = dir.rotated(Vector3(0,1,0), rotation.y)
+		
+	vel.x = dir.x
+	vel.z = dir.z
+	vel.y += GRAVITY * delta
+		
+	
+	vel = move_and_slide(vel, Vector3(0,1,0))
